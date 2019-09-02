050338a090954de0ee8d71485d2b9e7a9ac2397c
Ankit Tiwari <ankitt255@gmail.com>
2019-09-02 20:26:12 +0530

    Use react ace editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,96 +1,45 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
+import AceEditor from "react-ace";
 import styled from "styled-components";
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
+import "brace/mode/c_cpp";
+import "brace/theme/monokai";
 
 const StyledEditor = styled.div`
   border: 1px solid grey;
-
-  /* Syntax highlighting */
-  .token.comment,
-  .token.prolog,
-  .token.doctype,
-  .token.cdata {
-    color: #90a4ae;
-  }
-  .token.punctuation {
-    color: #9e9e9e;
-  }
-  .namespace {
-    opacity: 0.7;
-  }
-  .token.property,
-  .token.tag,
-  .token.boolean,
-  .token.number,
-  .token.constant,
-  .token.symbol,
-  .token.deleted {
-    color: #e91e63;
-  }
-  .token.selector,
-  .token.attr-name,
-  .token.string,
-  .token.char,
-  .token.builtin,
-  .token.inserted {
-    color: #4caf50;
-  }
-  .token.operator,
-  .token.entity,
-  .token.url,
-  .language-css .token.string,
-  .style .token.string {
-    color: #795548;
-  }
-  .token.atrule,
-  .token.attr-value,
-  .token.keyword {
-    color: #3f51b5;
-  }
-  .token.function {
-    color: #f44336;
-  }
-  .token.regex,
-  .token.important,
-  .token.variable {
-    color: #ff9800;
-  }
-  .token.important,
-  .token.bold {
-    font-weight: bold;
-  }
-  .token.italic {
-    font-style: italic;
-  }
-  .token.entity {
-    cursor: help;
-  }
 `;
 
-function MyEditor(props) {
-  return (
-    <StyledEditor className="editor">
-      <Editor
-        value={props.code}
-        onValueChange={props.onValueChange}
-        highlight={code => highlight(code, languages.js)}
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 14
-        }}
-      />
-    </StyledEditor>
-  );
+class MyEditor extends Component {
+  onLoad = editor => {
+    editor.textInput.focus();
+  };
+
+  render() {
+    return (
+      <StyledEditor className="editor">
+        <AceEditor
+          mode="c_cpp"
+          theme="monokai"
+          name="editor"
+          width="100%"
+          height="300px"
+          onLoad={this.onLoad}
+          onChange={this.props.onValueChange}
+          value={this.props.code}
+          fontSize={14}
+          showPrintMargin={false}
+          showGutter={true}
+          highlightActiveLine={true}
+          setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2
+          }}
+        />
+      </StyledEditor>
+    );
+  }
 }
 
 export default MyEditor;
-
-MyEditor.propTypes = {
-  code: PropTypes.string,
-  onValueChange: PropTypes.func
-};
